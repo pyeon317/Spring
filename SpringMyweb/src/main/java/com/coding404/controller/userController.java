054cf38addfcdda55b3b771b0cefdbf085940085
pyeon317 <pyeon317@gmail.com>
2023-07-25 17:10:53 +0900

    2023-07-25 Spring05 JinUng

---
+++ SpringMyweb/src/main/java/com/coding404/controller/userController.java
@@ -0,0 +1,25 @@
+package com.coding404.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class userController {
+
+	@RequestMapping("/join")
+	public String join() {
+		return "user/join";
+	}
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "user/login";
+	}
+	
+	@RequestMapping("/mypage")
+	public String mypage() {
+		return "user/mypage";
+	}
+	
+}
